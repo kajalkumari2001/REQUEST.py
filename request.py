c1c7d4898665788bfa6ef0f37a9976b444167514
kajalkumari2001 <100478697+kajalkumari2001@users.noreply.github.com>
2022-06-27 10:55:16 +0530

    Update request.py

--- request.py
+++ request.py
@@ -1,27 +1,32 @@
-import json
 import requests
-x=requests.get("http://saral.navgurukul.org/api/courses")
-h=x.json()
-with open("requestfile.json","w") as a:
-    json.dump(h,a,indent=4)
-i=0
-id=[]
-while i<len(h["availableCourses"]):
-    Name=h["availableCourses"][i]["name"]
-    Id=h["availableCourses"][i]["id"]
-    id.append(Id)
-    print(i," ",Name," ",Id)
-    i=i+1
-choose=int(input("enter the number:"))
-r=0
-k=requests.get("http://saral.navgurukul.org/api/courses/"+id[choose]+"/exercises")
-m=k.json()
-list2=[]
-for i in m["data"]:
-    print(r,i["slug"])
-    list2.append(i["slug"])
-    r=r+1
-choose2=int(input("enter the slug number"))
-n=requests.get("http://saral.navgurukul.org/api/courses/"+str(choose)+"exercises/getbyslug?slug="+list2[choose2])
-o=n.json()
-print(o)
+import json
+def request():
+    a = requests.get("http://saral.navgurukul.org/api/courses")
+    print("No. Courses---ID")
+    with open("courses.json","w") as f:
+        dict=json.loads(a.text)
+        json.dump(dict,f,indent=4)
+    with open("courses.json","r") as f:
+        data = json.load(f)
+    id= [] 
+    i = 0
+    while i < len(data['availableCourses']):
+        print(i,data['availableCourses'][i]['name'],"---",data['availableCourses'][i]['id'])
+        id.append(data['availableCourses'][i]['id'])
+        i+=1 
+    
+    user= int(input("**select the serial number:"))
+    ex=requests.get("http://saral.navgurukul.org/api/courses/"+str(id[user])+"/exercises")
+    a=ex.json()
+    j=0
+    l=0
+    slug=[]
+    while j<len(a["data"]):
+        print(l,a["data"][j]["name"])
+        slug.append(a['data'][j]["slug"])
+        l=l+1
+        j=j+1
+    slugname=int(input("**Enter your slug number:"))
+    sluglist=requests.get("http://saral.navgurukul.org/api/courses/"+ str(user)+"/exercise/getBySlug?slug=" + slug[slugname])
+    b=sluglist.json()
+    print("CONTENT",b["content"])
